c878a37a05d03a1bba971fbfa984eaf05165ba94
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2019-02-07 17:11:11 +0700

    Update non_tag.php

--- non_tag.php
+++ non_tag.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <title>Status images Tag</title>
+    <title>Tag Image</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel = "stylesheet" type = "text/css" href = "firn-style.css" >
+    <link rel = "stylesheet" type = "text/css" href = "style.css" >
     </head>
   <style>
       h2{
