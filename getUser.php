c55b0e38dc5f10d560e2585ae32716bd4d9e447d
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2019-02-25 13:49:00 +0700

    Update getUser.php

--- getUser.php
+++ getUser.php
@@ -2,7 +2,7 @@
 date_default_timezone_set(“Asia/Bangkok”);
 $date = date(“Y-m-d”);
 $time = date(“H:i:s”);
-$json = file_get_contents(‘php://input’);
+$json = file_get_contents(‘php:getUser.php’);
 $request = json_decode($json, true);
 $queryText = $request[“queryResult”][“queryText”];
 $userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
@@ -10,5 +10,4 @@ $myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
 $log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
 fwrite($myfile,$log);
 fclose($myfile);
-
 ?>
