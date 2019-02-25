819561afeb93d42a60e69f9a27c8c8844ea70c6c
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2019-02-25 16:31:11 +0700

    Update log.php

--- log.php
+++ log.php
@@ -2,7 +2,7 @@
 date_default_timezone_set(“Asia/Bangkok”);
 $date = date(“Y-m-d”);
 $time = date(“H:i:s”);
-$json = file_get_contents(‘php:getUser.php’);
+$json = file_get_contents(‘php://input’);
 $request = json_decode($json, true);
 $queryText = $request[“queryResult”][“queryText”];
 $userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
