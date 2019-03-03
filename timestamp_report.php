de289fb0170916574b77ee98b58cba8a5a9cdf28
firn <firnfroy@gmail.com>
2019-03-03 13:08:18 +0700

    หน้ารายงานการเข้าออกงาน

---
+++ timestamp_report.php
@@ -0,0 +1,35 @@
+้<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <title>Work Reports/title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel = "stylesheet" type = "text/css" href = "style.css" >
+</head>
+    <body>
+      <h2>รายงานเวลาการเข้างาน</h2>
+      <div class="container">
+      <form>
+          <div class="choose_status">
+            <label for="report">เลือกรายชื่อ</label>
+            <select  id="timestamp" name ="timestamp">
+              <option></option>
+              <option name="CP01">พี่ประวิม</option>
+              <option name="CP02">พี่แอน</option>
+              <option name="CP03">พี่หมาย</option>
+              <option name="CP04">พี่นพดล</option>
+              <option name="CP05">พี่สานนท์</option>
+            </select>
+            <input style="margin-top: 10px;" type="submit" name="send_report" value="ยืนยัน">
+          </div>
+      </form>
+    </div>
+    </body>
+</html>
+<?php
+
+
+?>
