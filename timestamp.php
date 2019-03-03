fcc1b465444eaa202586cce87121932364f9f750
firn <firnfroy@gmail.com>
2019-03-03 17:02:22 +0700

    commit

--- timestamp.php
+++ timestamp.php
@@ -1,4 +1,19 @@
 <?php
 $who_timestamp = $_GET['timestamp'];
-echo $who_timestamp."<br>";
+// echo $who_timestamp."<br>";
+
 ?>
+<html lang="en" dir="ltr">
+<head>
+  <title>Work Reports</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel = "stylesheet" type = "text/css" href = "style.css" >
+</head>
+<body>
+      <h2>รายงานเวลาการเข้างาน</h2><br>
+</body>
+</html>
