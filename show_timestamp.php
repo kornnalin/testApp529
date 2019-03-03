8ef90dad6fa0ee83794d788810a7cca42a40d22d
firn <firnfroy@gmail.com>
2019-03-04 01:55:36 +0700

    edit code

---
+++ show_timestamp.php
@@ -0,0 +1,19 @@
+<!-- ต้องแก้ไขใหม่ด้วยนะ -->
+<html lang="en" dir="ltr">
+<head>
+  <title>Work Reports</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel = "stylesheet" type = "text/css" href = "style.css" >
+</head>
+<body>
+      <h2>รายงานเวลาการเข้างาน</h2><br>
+      <center><h3>วันจันทร์ที่ 4 มีนาคม 2019</h3></center>
+      <h3>เข้างาน  : <b>08.00 น.</b></h3>
+      <h3>ออกงาน : <b>17.00 น.</b></h3>
+
+</body>
+</html>
