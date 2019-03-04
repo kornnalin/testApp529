7fd409d730fc3905ea7a0c39785ee14fc7f3a409
firn <firnfroy@gmail.com>
2019-03-05 01:05:19 +0700

    no message

--- show_timestamp.php
+++ show_timestamp.php
@@ -17,13 +17,59 @@ img{
 h2{
   color: blue;
 }
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+th{
+  background-color: #FFC900;
+  text-align: center;
+}
+td, th {
+  border: 1px solid #FFF089;;
+  text-align: left;
+  padding: 8px;
+}
+.firn{
+  text-align: center;
+  border: 1px solid #FFF089;;
+}
+
+tr:nth-child(even) {
+  background-color: #FFF089;;
+}
 </style>
 <body>
       <h2><b>รายงานเวลาการเข้างาน</b></h2><br>
-      <center><h3>วันจันทร์ที่ 3 มีนาคม 2562</h3></center>
-      <center><img src="2019-01-30_12_44_21.jpg" alt="img"></center>
-      <center><h4>เข้างาน  : <b>08.00 น.</b></h4></center>
-      <center><h4>ออกงาน : <b>17.00 น.</b></h4></center>
+      <center><h3>นางสาวกรณ์นลินล์ เชวงทรัพย์</h3></center>
+      <table>
+        <tr>
+          <th class="firn">วันที่</th>
+          <th class="firn">เวลาเข้า</th>
+          <th class="firn">เวลาออก</th>
+          <th class="firn">ภาพถ่าย</th>
+        </tr>
+        <tr>
+          <td>วันที่ 1 มีนาคม 2562</td>
+          <td>08:00</td>
+          <td>16:45</td>
+          <td><a href="2019-01-30_12_44_21.jpg">คลิ๊ก</a></td>
+        </tr>
+        <tr>
+          <td>วันที่ 2 มีนาคม 2562</td>
+          <td style="background-color:red;">08:45</td>
+          <td>16:30</td>
+          <td><a href="2019-01-30_12_44_21.jpg">คลิ๊ก</a></td>
+        </tr>
+        <tr>
+          <td>วันที่ 3 มีนาคม 2562</td>
+          <td>07:58</td>
+          <td style="background-color:red;">15:30</td>
+          <td><a href="2019-01-30_12_44_21.jpg">คลิ๊ก</a></td>
+        </tr>
+
+      </table>
 
 </body>
 </html>
