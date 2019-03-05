46497c52ac131e61a8f15595a112a987489b38af
firn <firnfroy@gmail.com>
2019-03-05 15:06:32 +0700

    edit

--- timestamp.php
+++ timestamp.php
@@ -1,7 +1,6 @@
 <?php
-$who_timestamp = $_GET['timestamp'];
+// $who_timestamp = $_GET['timestamp'];
 // echo $who_timestamp."<br>";
-
 ?>
 <html lang="en" dir="ltr">
 <head>
@@ -13,14 +12,28 @@ $who_timestamp = $_GET['timestamp'];
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link rel = "stylesheet" type = "text/css" href = "style.css" >
 </head>
+<style>
+input{
+  width: 25%;
+  border-color: gray;
+  padding: 3px;
+}
+</style>
 <body>
       <h2>รายงานเวลาการเข้างาน</h2><br>
       <h4>เลือกวันที่ต้องการดูจากด้านล่าง</h4>
+      <div class="row">
+        <center><form action="test.php" method="get">
+        <h3>เลือกวันที่วันแรก : <input type="date" name="first" ></h3><br><br>
+        <h3>เลือกวันสุดท้าย  : <input type="date" name="last"></h3><br><br>
+        <h3><input type="submit" value="ยืนยัน"></h3>
+      </form></center>
+      </div>
 </body>
 </html>
 <?php
-if ($who_timestamp) {
-  include 'calender.php';
-}
+// if ($who_timestamp) {
+//   include 'calender.php';
+// }
 ?>
 <!-- <button  style="text-align:center; margin-top: 10px; float: right;"><b><a href="show_timestamp.php">ยืนยัน</a></b></button> -->
