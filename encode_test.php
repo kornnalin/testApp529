ad9f107753fa5f84d177e632ffef7605c3786bee
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2019-09-27 09:16:24 +0700

    Update encode_test.php

--- encode_test.php
+++ encode_test.php
@@ -1,4 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Encode Decode</title>
+  </head>
+  <body>
+    <select name="type">
+        <option value="Encode"></option>
+        <option value="Decode"></option>
+    </select>
+    <form  action="encode_test.php" method="post">
+      Input :  <input type="text" name="str_encode">
+      <input type="submit" name="sub_send" value="Commit">
+    </form>
+
+  </body>
+</html>
+
 <?php
-$str = 'This is an encoded string';
-echo base64_encode($str);
+$type = $_REQUST['type'];
+$str = $_REQUST['str_encode'];
+if($type){
+  echo $type.'\n';
+}
+if ($str) {
+  $str = 'This is an encoded string';
+  echo base64_encode($str);
+}
+
 ?>
